aa2100d214b07da5a3bbca24658421dbcd342518
Aristides Papadopoulos <aristides.papadopoulos@gmail.com>
2017-04-06 15:12:13 +0200

    Renamed

--- src/main/java/app/we/go/utils/rx2/TransformerFactory.java
+++ src/main/java/app/we/go/utils/rx2/TransformerFactory.java
@@ -8,7 +8,7 @@ import io.reactivex.SingleTransformer;
  * Interface of factory for creating {@link Transformer}, typically to wrap the
  * <code>subscribeOn</code> and <code>observeOn</code> operators.
  */
-public interface TransformableFactory {
+public interface TransformerFactory {
 
     <U,D> ObservableTransformer<U, D> getObservableTransformer();
     <U,D> SingleTransformer<U, D> getSingleTransformer();
