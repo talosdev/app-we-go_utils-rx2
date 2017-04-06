227d0791a05e69e4f13e0149dbf382a17d9443f4
Aristides Papadopoulos <aristides.papadopoulos@gmail.com>
2017-04-06 17:57:39 +0200

    Javadoc and renames

---
+++ src/main/java/app/we/go/utils/rx2/SchedulersTransformerFactory.java
@@ -0,0 +1,17 @@
+package app.we.go.utils.rx2;
+
+import io.reactivex.CompletableTransformer;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.SingleTransformer;
+
+/**
+ * Interface for a factory that can create {@link Transformer} that can compose appropriate schedulers to an
+ * {@link io.reactivex.Observable}/{@link io.reactivex.Single}/{@link io.reactivex.Completable}.
+ */
+public interface SchedulersTransformerFactory {
+
+    <U> ObservableTransformer<U, U> forObservables();
+    <U> SingleTransformer<U, U> forSingle();
+    CompletableTransformer forCompletable();
+
+}
